18b4327b088ab4f24c6f9d83cc3f7142debebc93
Prashant <prashant.hule6999@gmail.com>
2021-05-27 09:19:58 +0530

    New Dashborad TC added

--- src/main/java/com/crm/qa/pages/DashboardPage.java
+++ src/main/java/com/crm/qa/pages/DashboardPage.java
@@ -4,13 +4,27 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import com.crm.qa.base.TestBase;
-import com.crm.qa.util.TestUtil;
 
 public class DashboardPage extends TestBase {
 
 	// Page Factory - OR:
 	@FindBy(xpath = "//a[contains(text(),'Home')]")
 	WebElement homeTab;
+
+	@FindBy(xpath = "//*[@title='Call']")
+	WebElement callTab;
+
+	@FindBy(xpath = "//*[@title='Reports']")
+	WebElement reportsTab;
+
+	@FindBy(xpath = "//td[@align='left'and @class='headertext']")
+	WebElement username;
+
+	@FindBy(xpath = "//td[@class='logo_text']")
+	WebElement logoText;
+
+	@FindBy(css = "div>.footnote")
+	WebElement country;
 
 	// Initializing the Page Objects:
 	public DashboardPage() {
@@ -19,8 +33,31 @@ public class DashboardPage extends TestBase {
 
 	// Actions:
 	public boolean validateHomeTab() {
-		TestUtil.switchToFrame();
 		boolean flag = homeTab.isDisplayed();
 		return flag;
+	}
+
+	public boolean validateCallTab() {
+		boolean flag = callTab.isDisplayed();
+		return flag;
+	}
+
+	public boolean validateReportsTab() {
+		boolean flag = reportsTab.isDisplayed();
+		return flag;
+	}
+
+	public String validateUsername() {
+		return username.getText().trim();
+	}
+
+	public String validateLogoText() {
+		return logoText.getText().trim();
+	}
+
+	public String validateCountry() {
+		String str[] = country.getText().split(" ");
+		String str1[] = str[7].split("\n");
+		return str1[0].trim();
 	}
 }
--- src/test/java/com/crm/qa/testcases/DashboardPageTest.java
+++ src/test/java/com/crm/qa/testcases/DashboardPageTest.java
@@ -3,11 +3,15 @@ package com.crm.qa.testcases;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import com.crm.qa.base.TestBase;
 import com.crm.qa.pages.DashboardPage;
 import com.crm.qa.pages.LoginPage;
+import com.crm.qa.util.TestAllureListener;
+import com.crm.qa.util.TestUtil;
 
+@Listeners(TestAllureListener.class)
 public class DashboardPageTest extends TestBase {
 	LoginPage loginPage;
 	DashboardPage dashboardPage;
@@ -22,12 +26,43 @@ public class DashboardPageTest extends TestBase {
 		loginPage = new LoginPage();
 		loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
 		dashboardPage = new DashboardPage();
+		TestUtil.switchToFrame();
 	}
 
 	@Test(priority = 4)
-	public void validateUserTest() {
+	public void validateHomeTabTest() {
 		boolean acutalResult = dashboardPage.validateHomeTab();
 		Assert.assertTrue(acutalResult, "Home Tab is not Present on CRM Dashboard");
+	}
+
+	@Test(priority = 5)
+	public void validateCallTabTest() {
+		boolean acutalResult = dashboardPage.validateCallTab();
+		Assert.assertTrue(acutalResult, "Call Tab is not Present on CRM Dashboard");
+	}
+
+	@Test(priority = 6)
+	public void validateReportsTabTest() {
+		boolean acutalResult = dashboardPage.validateReportsTab();
+		Assert.assertTrue(acutalResult, "Reports Tab is not Present on CRM Dashboard");
+	}
+
+	@Test(priority = 7)
+	public void validateUsernameTest() {
+		String acutalResult = dashboardPage.validateUsername();
+		Assert.assertEquals(acutalResult, "User: Mukesh Otwani", "User Name Mismatch");
+	}
+
+	@Test(priority = 8)
+	public void validateLogoTextTest() {
+		String acutalResult = dashboardPage.validateLogoText();
+		Assert.assertEquals(acutalResult, "CRMPRO", "Logo Text Mismatch");
+	}
+
+	@Test(priority = 9)
+	public void validateCountryTest() {
+		String acutalResult = dashboardPage.validateCountry();
+		Assert.assertEquals(acutalResult, "USA", "Country Mismatch");
 	}
 
 	@AfterClass
